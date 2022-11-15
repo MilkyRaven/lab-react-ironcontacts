11211297cb7cd610c812d5310ecd5669d53c3f6a
Milky Raven <heartonllamas@gmail.com>
2022-11-15 16:56:31 +0100

    iteration one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        <tr>
+          <td><img width={100} src={contacts[0].pictureUrl} alt="" ></img> </td>
+          <td>{contacts[0].name}</td>
+          <td>{contacts[0].popularity}</td>
+        </tr>
+        <tr>
+          <td><img width={100} src={contacts[1].pictureUrl} alt=""></img> </td>
+          <td>{contacts[1].name}</td>
+          <td>{contacts[1].popularity}</td>
+        </tr>
+        <tr>
+          <td><img width={100} src={contacts[2].pictureUrl} alt=""></img> </td>
+          <td>{contacts[2].name}</td>
+          <td>{contacts[2].popularity}</td>
+        </tr>
+        <tr>
+          <td><img width={100} src={contacts[3].pictureUrl} alt=""></img> </td>
+          <td>{contacts[3].name}</td>
+          <td>{contacts[3].popularity}</td>
+        </tr>
+        <tr>
+          <td><img width={100} src={contacts[4].pictureUrl} alt=""></img> </td>
+          <td>{contacts[4].name}</td>
+          <td>{contacts[4].popularity}</td>
+        </tr>
+      </table>
     </div>
   );
 }
